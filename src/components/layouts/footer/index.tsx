424f06a7e78bfb98c23064bd1ba9b67442ac408e
FatahRozaq <fatah.rozaq20@gmail.com>
2025-01-28 20:33:17 +0700

    Update footer

---
+++ src/components/layouts/footer/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface FooterProps {
+
+}
+
+const Footer: FC<FooterProps> = ({}) => {
+    return (
+        <div className="w-full">
+            Copyright &copy; 2024 Fatah Rozaq
+        </div>
+    )
+}
+
+export default Footer;
